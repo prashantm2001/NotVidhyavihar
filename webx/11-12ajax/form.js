5d0e8f124c96fb00d816dac1f81ed83a191beb10
haider <haider.n@somaiya.edu>
2022-05-06 01:20:31 +0530

    Nicely done!

---
+++ webx/11-12ajax/form.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    event.preventDefault();
+      $(".form-group").removeClass("has-error");
+      $(".help-block").remove();
+    var formData = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+      superheroAlias: $("#superheroAlias").val(),
+    };
+console.log(formData);
+    $.ajax({
+      type: "post",
+      url: "http://127.0.0.1:8000/",
+      data: JSON.stringify(formData),
+      contentType: "application/json; charset=utf-8",
+      traditional: true,
+    }).done(function (data) {
+      console.log(data);
+
+      if (!data.success) {
+        if (data.errors.name) {
+          $("#name-group").addClass("has-error");
+          $("#name-group").append(
+            '<div class="help-block">' + data.errors.name + "</div>"
+          );
+        }
+
+        if (data.errors.email) {
+          $("#email-group").addClass("has-error");
+          $("#email-group").append(
+            '<div class="help-block">' + data.errors.email + "</div>"
+          );
+        }
+
+        if (data.errors.superheroAlias) {
+          $("#superhero-group").addClass("has-error");
+          $("#superhero-group").append(
+            '<div class="help-block">' + data.errors.superheroAlias + "</div>"
+          );
+        }
+      } else {
+        console.log(data.message);
+        $("form").html(
+          '<div class="alert alert-success">' + data.message + "</div>"
+        );
+      }
+
+    })
+    .fail(function (data) {
+      console.log(data);
+        $("form").html(
+          '<div class="alert alert-danger">Could not reach server, please try again later.</div>'
+        );
+      });
+
+    event.preventDefault();
+  });
+});
---
+++ webx/11-12ajax/main.py
@@ -0,0 +1,60 @@
+from typing import Optional
+import re
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+class Person(BaseModel):
+    name: str
+    email: str
+    superheroAlias: str
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def validate_name(name: str) -> str:
+    if name == "":
+        return "Name is required"
+    elif len(name) < 3:
+       return  "Name must be at least 3 characters long"
+    else:
+      return None;            
+def validate_email(email: str) -> str:
+  regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+  if email == "":
+    return "Email is required"
+  elif not re.search(regex, email):
+    return "Email is not valid"
+  else:
+    return None;
+@app.post("/")
+def read_root(person : Person):
+    data = {
+      "success" : False,
+      "message" : ""
+    }
+    errors = {
+      "name": "",
+      "email": "",
+      "superheroAlias": "",
+    }
+    errors["name"]= validate_name(person.name)
+    errors["email"]= validate_email(person.email)
+    errors["superheroAlias"]= validate_name(person.superheroAlias)
+    print(errors)
+    if errors["name"] or errors["email"] or errors["superheroAlias"]:
+      print("errors")
+      data["success"] = False
+      data["errors"] = errors
+    else:
+      data["success"] = True
+      data["message"] = "Nicely done!"
+      
+      
+    return data
